30d87ebccf13ba4d89aa82e027b3903d7cafb41a
Jon Fuchs <jonfox@Jons-MacBook-Pro.local>
2018-11-01 16:47:08 -0600

    added auth_protocal

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.get('/auth/callback', async (req, res) => {
         client_secret: CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: AUTH_PROTOCAL
+        redirect_uri: `${AUTH_PROTOCAL$}://{req.headers.host}/auth/callback`
     }
     // post request with code for token
     let tokenRes = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload);
@@ -105,7 +105,7 @@ app.get('/auth/callback/admin', async (req, res) => {
         client_secret: CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `http://${req.headers.host}/auth/callback`
+        redirect_uri: `${AUTH_PROTOCAL}://${req.headers.host}/auth/callback`
     }
     // post request with code for token
     let tokenRes = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload);
